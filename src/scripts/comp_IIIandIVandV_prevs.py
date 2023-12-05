41217d062731dfb47d9ca0a71a035839003489b0
rmnldwg <48687784+rmnldwg@users.noreply.github.com>
2023-12-05 11:59:01 +0100

    change plots to more relevant with new win graph

--- src/scripts/comp_IIIandIVandV_prevs.py
+++ src/scripts/comp_IIIandIVandV_prevs.py
@@ -1,6 +1,6 @@
 """
 Plot histograms of predicted and posteriors over observed prevalences for scenarios
-that involve the LNLs II, III, and V. Compare these prevalence predictions for the
+that involve the LNLs III, IV, and V. Compare these prevalence predictions for the
 base-graph model to the winning-graph model.
 """
 from pathlib import Path
@@ -27,26 +27,26 @@ if __name__ == "__main__":
     plt.rcParams.update(fontsizes.icml2022())
     fig, axes = plt.subplots(nrows=NROWS, ncols=NCOLS, sharex="col")
 
-    # left panels: prevalences of LNLs II and V
-    BASE_GRAPH_INPUT = paths.data / "bg_IIandV_prevs.hdf5"
-    WIN_GRAPH_INPUT = paths.data / "wg_IIandV_prevs.hdf5"
+    # prevalences of LNLs IV and V
+    BASE_GRAPH_INPUT = paths.data / "bg_IVandV_prevs.hdf5"
+    WIN_GRAPH_INPUT = paths.data / "wg_IVandV_prevs.hdf5"
 
-    left_panels = {
+    panels_IVandV = {
         "early": [],
         "late": [],
     }
 
     for stage in ["early", "late"]:
-        left_panels[stage] = [
+        panels_IVandV[stage] = [
             Histogram.from_hdf5(
                 filename=WIN_GRAPH_INPUT,
-                dataname=f"IIandV/{stage}",
-                label="LNL II and V",
+                dataname=f"IVandV/{stage}",
+                label="LNL IV and V",
                 color=USZ["orange"],
             ),
             Histogram.from_hdf5(
                 filename=BASE_GRAPH_INPUT,
-                dataname=f"IIandV/{stage}",
+                dataname=f"IVandV/{stage}",
                 label="base graph",
                 histtype="step",
                 color="darkgray",
@@ -55,19 +55,19 @@ if __name__ == "__main__":
             ),
             Posterior.from_hdf5(
                 filename=BASE_GRAPH_INPUT,
-                dataname=f"IIandV/{stage}",
+                dataname=f"IVandV/{stage}",
                 color=USZ["orange"],
             ),
 
             Histogram.from_hdf5(
                 filename=WIN_GRAPH_INPUT,
-                dataname=f"VnotII/{stage}",
-                label="LNL V without II",
+                dataname=f"VnotIV/{stage}",
+                label="LNL V without IV",
                 color=USZ["blue"],
             ),
             Histogram.from_hdf5(
                 filename=BASE_GRAPH_INPUT,
-                dataname=f"VnotII/{stage}",
+                dataname=f"VnotIV/{stage}",
                 label="base graph",
                 histtype="step",
                 color="darkgray",
@@ -76,35 +76,22 @@ if __name__ == "__main__":
             ),
             Posterior.from_hdf5(
                 filename=BASE_GRAPH_INPUT,
-                dataname=f"VnotII/{stage}",
+                dataname=f"VnotIV/{stage}",
                 color=USZ["blue"],
             ),
         ]
 
-    draw(axes[0,0], contents=left_panels["early"], xlims=(0., 12.), hist_kwargs={"nbins": 40})
-    axes[0,0].set_ylim(bottom=0., top=0.9)
-    axes[0,0].set_ylabel("early T-cat.", fontweight="bold")
-    axes[0,0].set_yticks([])
-    axes[0,0].legend()
-
-    draw(axes[1,0], contents=left_panels["late"], xlims=(0., 12.), hist_kwargs={"nbins": 40})
-    # axes[1,0].set_ylim(bottom=0., top=1.2)
-    axes[1,0].set_ylabel("late T-cat.", fontweight="bold")
-    axes[1,0].set_yticks([])
-    axes[1,0].set_xlabel("prevalence [%]")
-    axes[1,0].legend(handles=[h for h in axes[1,0].get_children() if isinstance(h, Line2D)])
-
-    # right panels: prevalences of III and V
+    # prevalences of III and V
     BASE_GRAPH_INPUT = paths.data / "bg_IIIandV_prevs.hdf5"
     WIN_GRAPH_INPUT = paths.data / "wg_IIIandV_prevs.hdf5"
 
-    right_panels = {
+    panels_IIIandV = {
         "early": [],
         "late": [],
     }
 
     for stage in ["early", "late"]:
-        right_panels[stage] = [
+        panels_IIIandV[stage] = [
             Histogram.from_hdf5(
                 filename=WIN_GRAPH_INPUT,
                 dataname=f"IIIandV/{stage}",
@@ -148,13 +135,27 @@ if __name__ == "__main__":
             ),
         ]
 
-    draw(axes[0,1], contents=right_panels["early"], xlims=(0., 8.), hist_kwargs={"nbins": 40})
-    axes[0,1].set_ylim(bottom=0., top=0.9)
+    draw(axes[0,0], contents=panels_IIIandV["early"], xlims=(0., 12.), hist_kwargs={"nbins": 40})
+    axes[0,0].set_ylim(bottom=0., top=1.0)
+    axes[0,0].set_ylabel("early T-cat.", fontweight="bold")
+    axes[0,0].set_yticks([])
+    axes[0,0].legend()
+
+    draw(axes[1,0], contents=panels_IIIandV["late"], xlims=(0., 12.), hist_kwargs={"nbins": 40})
+    # axes[1,0].set_ylim(bottom=0., top=1.2)
+    axes[1,0].set_ylabel("late T-cat.", fontweight="bold")
+    axes[1,0].set_yticks([])
+    axes[1,0].set_xlabel("prevalence [%]")
+    axes[1,0].legend(handles=[h for h in axes[1,0].get_children() if isinstance(h, Line2D)])
+
+
+    draw(axes[0,1], contents=panels_IVandV["early"], xlims=(0., 12.), hist_kwargs={"nbins": 40})
+    axes[0,1].set_ylim(bottom=0., top=1.0)
     # axes[0,1].set_ylabel("early T-cat.", fontweight="bold")
     axes[0,1].set_yticks([])
     axes[0,1].legend()
 
-    draw(axes[1,1], contents=right_panels["late"], xlims=(0., 8.), hist_kwargs={"nbins": 40})
+    draw(axes[1,1], contents=panels_IVandV["late"], xlims=(0., 12.), hist_kwargs={"nbins": 40})
     # axes[1,1].set_ylim(bottom=0., top=1.2)
     # axes[1,1].set_ylabel("late T-cat.", fontweight="bold")
     axes[1,1].set_yticks([])
