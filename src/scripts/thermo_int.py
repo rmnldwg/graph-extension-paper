d4520d55d2b61fe351eaf5ede30976dc6854080b
Roman Ludwig <48687784+rmnldwg@users.noreply.github.com>
2024-05-13 12:43:26 +0200

    show diff of TI rounds

--- src/scripts/thermo_int.py
+++ src/scripts/thermo_int.py
@@ -18,14 +18,14 @@ ACCURACY_PATHS = {
     "win" : paths.data / "win_accuracies.csv",
 }
 OUTPUT = (paths.figures / Path(__file__).name).with_suffix(".png")
-POWER = 5
+POWER = 3
 
 
 if __name__ == "__main__":
     plt.rcParams.update(figsizes.icml2022_half())
     plt.rcParams.update(fontsizes.icml2022())
     plt.rcParams.update({"text.usetex": False})
-    fig, axes = plt.subplots()
+    fig, axes = plt.subplots(nrows=2, ncols=1, sharex=True)
 
     accuracies = pd.read_csv(
         ACCURACY_PATHS["lose"]
@@ -42,30 +42,40 @@ if __name__ == "__main__":
         lsuffix="_",
     )
 
-    errors = accuracies.copy().drop(
-        columns=["lose graph", "win graph"]
-    ).rename(
-        columns={"std_": "lose graph", "std": "win graph"}
-    )
-    errors["β"] = errors["β"] ** (1./POWER)
     accuracies = accuracies.drop(columns=["std_", "std"])
     accuracies["β"] = accuracies["β"] ** (1./POWER)
+    accuracies["diff"] = accuracies["win graph"] - accuracies["lose graph"]
 
     accuracies.plot(
-        x="β", y=["lose graph", "win graph"], ax=axes,
-        color=[USZ["red"], USZ["green"]],
+        x="β",
+        y="win graph",
+        ax=axes[0],
+        color=USZ["blue"],
+        label="winning graph",
     )
+    accuracies.plot(
+        x="β",
+        y="diff",
+        ax=axes[1],
+        color=USZ["red"],
+        label="winning - losing graph",
+    )
+    axes[1].axhline(0., color="black", linestyle="--")
 
     xticks = np.linspace(0., 1., 7)
     xticklabels = [f"{x**POWER:.2g}" for x in xticks]
-    axes.set_xticks(xticks)
-    axes.set_xticklabels(xticklabels)
-    axes.set_xlim(left=0., right=1.)
-    axes.set_xlabel(r"inverse temperature $\beta$")
-    axes.set_yscale("symlog",)
-    axes.get_yaxis().set_major_locator(matplotlib.ticker.MultipleLocator(800))
-    axes.get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
-    axes.ticklabel_format(axis="y", style="sci", scilimits=(2,2))
-    axes.set_ylabel(r"$\ln \mathcal{A}_{MC}(\beta)$")
 
+    axes[0].set_yscale("symlog",)
+    axes[0].get_yaxis().set_major_locator(matplotlib.ticker.MultipleLocator(900))
+    axes[0].get_yaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
+    axes[0].ticklabel_format(axis="y", style="sci", scilimits=(3,3))
+    axes[0].set_ylabel(r"$\ln \mathcal{A}_{MC}(\beta)$")
+
+    axes[1].set_ylabel(r"$\Delta \ln \mathcal{A}_{MC}(\beta)$")
+    axes[1].set_xticks(xticks)
+    axes[1].set_xticklabels(xticklabels)
+    axes[1].set_xlim(left=0., right=1.)
+    axes[1].set_xlabel(r"inverse temperature $\beta$")
+
+    fig.align_ylabels(axes)
     fig.savefig(OUTPUT, dpi=400)
